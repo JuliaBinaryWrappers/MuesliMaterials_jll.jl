27611bdf7c247a4a6b155c8e6aed24d2ac38a588
jlbuild <juliabuildbot@gmail.com>
2026-02-03 16:00:12 +0000

    MuesliMaterials_jll build 1.16.1+0

--- src/MuesliMaterials_jll.jl
+++ src/MuesliMaterials_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MuesliMaterials")
-JLLWrappers.@generate_main_file("MuesliMaterials", UUID("ef259003-9f3a-5fc7-ae68-dce6b88dc7d6"))
+JLLWrappers.@generate_main_file("MuesliMaterials", Base.UUID("ef259003-9f3a-5fc7-ae68-dce6b88dc7d6"))
 end  # module MuesliMaterials_jll
